212cc58b5af991d73b619fffbfd6caf758bb943e
lab7989 <lab7989@caelumBSB01-03>
2018-11-07 09:57:38 -0200

    Added sessão to ingressos

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,23 +2,42 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
 
+	@Id
+	@GeneratedValue
 	private Integer id;
+
 	private	LocalTime horario;
+
+	@ManyToOne
 	private Sala sala;
+
+	@ManyToOne
 	private Filme filme;
-	
+
+	/**
+	 * @deprecated hibernate only
+	 */
+	public Sessao() {
+	}
+
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
 	}
-	
+
 	public LocalTime getHorarioTermino() {
 		return this.horario.plusMinutes(filme.getDuracao().toMinutes());
 	}
-	
+
 	public Integer getId() {
 		return id;
 	}
@@ -43,6 +62,6 @@ public class Sessao {
 	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}
-	
-	
+
+
 }
